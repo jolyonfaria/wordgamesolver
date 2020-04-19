61bdbd1597722c19df0f6bebe086c3d584baea12
jolyon_faria <jolyon_faria@yahoo.co.uk>
2020-04-19 14:30:20 +0100

    {JSF} Initialise tests.

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(wordsearchgame)
+
+test_check("wordsearchgame")
---
+++ tests/testthat/test_check.R
@@ -0,0 +1,9 @@
+context("Check tests")
+
+test_that("test 1", {
+  expect_true(TRUE)
+})
+
+test_that("test 2", {
+  expect_true(TRUE)
+})
