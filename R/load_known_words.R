bac3f8c85235f787d7a3350088638a717a692ddb
jolyon_faria <jolyon_faria@yahoo.co.uk>
2020-05-23 10:48:22 +0100

    {JSF} Expose number of words as an option to change.

--- R/load_known_words.R
+++ R/load_known_words.R
@@ -3,13 +3,14 @@
 #' @description Load known words from file.
 #'
 #' @param known_words_file Known words file path
+#' @param n_words Number of words to use
 #'
 #' @export
-load_known_words <- function(known_words_file) {
+load_known_words <- function(known_words_file, n_words) {
   extra_words <- "arctic"
   stopifnot(file.exists(known_words_file))
   known_words <- utils::read.table(known_words_file, stringsAsFactors = FALSE)
-  known_words <- known_words[order(known_words$V2, decreasing = TRUE), ][1:100000,]
+  known_words <- known_words[order(known_words$V2, decreasing = TRUE), ][1:n_words,]
   known_words <- rbind(known_words, data.frame(V1 = extra_words, V2 = 1000))
   row.names(known_words) <- NULL
   return(known_words)
