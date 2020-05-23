eaea1eadac2ec51f3a766b6e47b9f1248c1f6b01
jolyon_faria <jolyon_faria@yahoo.co.uk>
2020-05-23 11:07:45 +0100

    {JSF} Update documentation in analysis file.

--- analysis/run_analysis.R
+++ analysis/run_analysis.R
@@ -5,9 +5,11 @@
 # (2) Word lengths
 # (3) Word starting letters (if any)
 
+# To user also needs to ensure that there is a known words file at location `word_freq_file`.
+
 # To run this process:
 # (1) Enter user inputs
-# (2) Run file
+# (2) Run file in an R environment e.g. RStudio
 
 # User input 1: Input letter matrix
 letter_matrix <- matrix(
@@ -27,11 +29,11 @@ word_lengths <- c(6)
 # User input 3: Word lengths and starting letters
 word_letters <- c("b")
 
-# Find k-mers within letter matrix
-mers_tree <- make_mers_tree(letter_matrix)
-
 # Load known words
 known_words <- load_known_words(word_freq_file)
+
+# Find k-mers within letter matrix
+mers_tree <- make_mers_tree(letter_matrix)
 
 # Find possible words
 possible_words <- find_possible_words(mers_tree, known_words, word_lengths, word_letters)
